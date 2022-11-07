694358d935dc973b2095b4a9e774ead6cd799539
bakhler <thehenridit@gmail.com>
2022-11-07 10:41:12 +0200

    Add examler conversion

---
+++ index.js
@@ -0,0 +1,11 @@
+let number1 = '12';
+let number2 = '12';
+
+console.log(number1+number2);
+console.log(number1+Number(number2));
+
+number1 = 5;
+number2 = 'qwert';
+
+console.log(number1*number2)
+
